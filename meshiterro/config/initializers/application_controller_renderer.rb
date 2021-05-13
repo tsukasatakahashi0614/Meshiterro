d1f580909039dae4f4ada6f7ab85223de9673fa8
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-05-13 09:00:38 +0000

    部分テンプレート化

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a5a7c03d4d210dd4e68197354095b4414eae6967593e8a94c41bd7a1ab5a421347c578bdf7b49d0f589a6efb79905ae190e6cba9e6a2d218e3aadcee06f8743a'
